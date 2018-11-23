0ebb9879433b65f1b3ec7ac337f52346c1bbaa6a
DUTTA Prasun <s0928794@ed.ac.uk>
2018-11-23 16:38:10 +0000

    XP-EHH scores Manhattan and QQ plot making scripts

---
+++ XP-EHH_manhattan_FDR_plotmaker.R
@@ -0,0 +1,55 @@
+library(readr)
+library(ggplot2)
+library(ggpubr)
+library(stringr) #for str_sort
+library(R.utils) #for listDirectory
+library(data.table) ##for rbindlist
+library(qqman)
+options(scipen = 999)
+
+setwd("~/Thesis_PhD/Graphs_tables_for_thesis/xpehh/")
+
+dir = listDirectory(pattern = "*_vs_*")
+xpehh_tables = list()
+
+for (i in 1:length(dir)) {
+  filenames = str_sort(list.files(dir[i],pattern = "*_vs_*"), numeric = T)
+  for (j in 1:length(filenames)) {
+    dt = read_delim(
+      paste0(dir[i],"/",filenames[j]),
+      "\t",
+      escape_double = FALSE,
+      col_types = cols(
+        iHH_A1 = col_skip(),
+        iHH_B1 = col_skip(),
+        iHH_P1 = col_skip()
+      ),
+      trim_ws = TRUE
+    )
+    dt$chrom = j
+    dt$SNP_ID = paste(dt$chrom, dt$Location, sep = "_")
+    dt$z_score = scale(dt$XPEHH, center = T, scale = T)
+    dt$z_score_abs = abs(dt$z_score)
+    dt$p_value = (1 - pnorm(dt$z_score_abs)) * 2
+    dt$FDR_adj_p_value=p.adjust(dt$p_value,method="BH",n=length(dt$p_value))
+    xpehh_tables[[j]] = dt
+  }
+  dt_final= rbindlist(xpehh_tables)
+  temp_dt=subset(dt_final,dt_final$FDR_adj_p_value <=0.05)
+  z=temp_dt$z_score[which.max(temp_dt$FDR_adj_p_value)]
+  png(paste0(dir[i],"_FDR_line",".png"), units="in", width=25, height=11, res=600)
+  manhattan(
+    dt_final,
+    chr = "chrom",
+    bp = "Location",
+    p = "z_score",
+    snp = "SNP_ID",
+    logp = FALSE,
+    ylim = c(min(dt_final$z_score) - 0.5, max(dt_final$z_score) + 0.5),
+    ylab = "XPEHH Z-scores",
+    genomewideline = z,
+    suggestiveline = -z,
+    main = dir[i]
+  )
+  dev.off()
+}
---
+++ qq_plot_maker.R
@@ -0,0 +1,50 @@
+library(readr)
+library(ggplot2)
+library(ggpubr)
+library(stringr) #for str_sort
+library(R.utils) #for listDirectory
+library(data.table) ##for rbindlist
+library(qqman)
+options(scipen = 999)
+
+#"setwd("~/Thesis_PhD/Graphs_tables_for_thesis/xpehh/")Banni_vs_Med/")
+#setwd("~/Thesis_PhD/Graphs_tables_for_thesis/xpehh/")
+#tiff("all.tiff", units="in", width=25, height=11, res=100)
+#par(mfrow=c(2,2))
+dir = listDirectory(pattern = "*_vs_*")
+xpehh_tables = list()
+
+for (i in 1:length(dir)) {
+  filenames = str_sort(list.files(dir[i], pattern = "*_vs_*"), numeric = T)
+  for (j in 1:length(filenames)) {
+    dt = read_delim(
+      paste0(dir[i], "/", filenames[j]),
+      "\t",
+      escape_double = FALSE,
+      col_types = cols(
+        iHH_A1 = col_skip(),
+        iHH_B1 = col_skip(),
+        iHH_P1 = col_skip()
+      ),
+      trim_ws = TRUE
+    )
+    dt$chrom = j
+    dt$SNP_ID = paste(dt$chrom, dt$Location, sep = "_")
+    dt$z_score = scale(dt$XPEHH, center = T, scale = T)
+    dt$z_score_abs = abs(dt$z_score)
+    dt$p_value = (1 - pnorm(dt$z_score_abs)) * 2
+    #dt = dt[, c(3,1,4,2,5,6)]
+    xpehh_tables[[j]] = dt
+  }
+  dt_final = rbindlist(xpehh_tables)
+  png(
+    paste0(dir[i], "_QQ_plot", ".png"),
+    units = "in",
+    width = 15,
+    height = 11,
+    res = 600
+  )
+  qq(dt_final$p_value, main = dir[i], col = "blue4")
+  dev.off()
+}
+
